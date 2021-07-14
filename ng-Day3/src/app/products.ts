e25c157de0804bbb9ca04a424e80a55402c455eb
ateyaf <ateyafkareem2@gmail.com>
2021-07-14 16:07:13 +0200

    ng-day3

---
+++ ng-Day3/src/app/products.ts
@@ -0,0 +1,35 @@
+export const products = [
+
+  {
+
+    name: 'Falafel',
+
+    price: 3,
+    img:"https://www.indianhealthyrecipes.com/wp-content/uploads/2019/05/falafel-recipe.jpg",
+
+    description: 'a staple Middle Eastern dish—and a popular street food around the world'
+
+  },
+
+  {
+
+    name: 'Shawarma',
+
+    price: 4,
+    img:"https://www.recipetineats.com/wp-content/uploads/2014/12/Chicken-Shawarma_5.jpg",
+
+    description: 'is a Levantine Arab meat preparation, where lamb, chicken, turkey, beef, veal, or mixed meats'
+
+  },
+
+  {
+
+    name: 'Baklava',
+
+    price: 20,
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAX8ph7uvUEWCDPcmikKru8gtBf_IqiOSC6Q&usqp=CAU",
+
+    description : ' is a delicious, flaky dessert made with thin sheets of dough soaked in honey and layered with nuts'
+  },
+
+ ];
